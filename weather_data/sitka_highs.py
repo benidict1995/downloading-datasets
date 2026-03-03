04630afe61e8bda3db6029b22f8d85e71aa0779d
Benidict Dulce <benidict@metromart.com>
2026-03-03 21:55:09 +0800

    initial for downloading datasets

---
+++ weather_data/sitka_highs.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+import csv
+import matplotlib.pyplot as plt
+
+path = Path('../csv_data/sitka_weather_2021_simple.csv')
+lines = path.read_text().splitlines()
+
+reader = csv.reader(lines)
+header_row = next(reader)
+
+for index, column_header in enumerate(header_row):
+    print(index, column_header)
+
+# Extract high temperatures.
+highs = []
+for row in reader:
+    high = int(row[4])
+    highs.append(high)
+
+# print(highs)
+
+# Plot the high temperatures.
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+
+# Format plot
+ax.set_title("Daily High Temperatures, July 2021", fontsize = 24)
+ax.set_xlabel('', fontsize = 16)
+ax.set_ylabel("Temperature (°F)", fontsize = 16)
+ax.tick_params(labelsize=16)
+plt.show()
